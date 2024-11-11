42120c7a780bb217259a177d277064e1f52a0215
Marlo Bruder <m.b.8@web.de>
2024-11-11 18:49:30 +0100

    Added NavSidebar component

---
+++ src/lib/navigation/NavSidebar.svelte
@@ -0,0 +1,6 @@
+<div class="h-full border-8 border-red-600">
+  <div class="h-full w-12 float-left border border-blue-600"></div>
+  <div class="ml-12">
+    <p>This is the nav sidebar in a custom component</p>
+  </div>
+</div>
--- src/routes/main/+layout.svelte
+++ src/routes/main/+layout.svelte
@@ -1,12 +1,14 @@
-<script>
+<script lang="ts">
+  import NavSidebar from "$lib/navigation/NavSidebar.svelte";
+
 	let { children } = $props();
 </script>
 
 <div class="h-screen w-screen">
-  <div class="h-full w-56 float-left border-8 border-red-600">
-    <p>This is the side bar</p>
+  <div class="h-full w-80 fixed top-0 left-0">
+    <NavSidebar></NavSidebar>
   </div>
-  <div class="h-full ml-56">
+  <div class="h-full ml-80">
     {@render children()}
   </div>
 </div>
