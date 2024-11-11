7a901875bb6e04d108044cfd8fad8a5e5fad7cf7
Marlo Bruder <m.b.8@web.de>
2024-11-11 06:33:08 +0100

    Home page added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <main>
-  <div class="m-40">
-    <h1 class="text-center text-4xl">Welcome to mmdbt!</h1>
+  <div class="m-40 text-center">
+    <h1 class="text-4xl">Welcome to mmdbt!</h1>
+    <a href="/home" class="inline-block mt-4">Start new Metamath database</a>
   </div>
 </main>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,3 @@
+<div>
+  <h1>THIS IS THE HOME PAGE</h1>
+</div>
