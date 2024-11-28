c0ff3f5f7efddb0e9a5c714ce711894608181e59
Marlo Bruder <m.b.8@web.de>
2024-11-28 01:06:44 +0100

    Created database module

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,24 +1,11 @@
-use diesel::prelude::*;
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn create_database(file_path: &str) {
-    //println!("Trying to create db with path {}!", file_path);
-    SqliteConnection::establish(&file_path)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", file_path));
-}
+mod database;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet, create_database])
+        .invoke_handler(tauri::generate_handler![database::create_database])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
