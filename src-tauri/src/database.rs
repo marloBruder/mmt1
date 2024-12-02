ae8c3fb8681791d2cb780b02ebd6a2f73e83dd90
Marlo Bruder <m.b.8@web.de>
2024-12-02 18:37:27 +0100

    Removed Diesel

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,8 +1,6 @@
-use diesel::prelude::*;
+
 
 #[tauri::command]
 pub fn create_database(file_path: &str) {
     //println!("Trying to create db with path {}!", file_path);
-    SqliteConnection::establish(&file_path)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", file_path));
 }
