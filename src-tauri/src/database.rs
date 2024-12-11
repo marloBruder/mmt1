549172f36ce1bfad49390cfb6e7f107e0b1800f3
Marlo Bruder <m.b.8@web.de>
2024-12-11 20:27:42 +0100

    sqlx create database command

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,6 +1,48 @@
+use sqlx::{migrate::MigrateDatabase, Sqlite};
+use std::fmt;
 
-
+// Tauri Command for creating a new database
+// If the database already exists, it will instead return an DatabaseExistsError
 #[tauri::command]
-pub fn create_database(file_path: &str) {
+pub async fn create_database(file_path: &str) -> Result<(), Error> {
     //println!("Trying to create db with path {}!", file_path);
+    if Sqlite::database_exists(file_path).await.unwrap_or(false) {
+        return Err(Error::DatabaseExistsError);
+    }
+    create_or_override_database_pure(file_path).await
+}
+
+// Tauri Command for creating a new database or overriding it if it already exists
+#[tauri::command]
+pub async fn create_or_override_database(file_path: &str) -> Result<(), Error> {
+    create_or_override_database_pure(file_path).await
+}
+
+// The function that actually creates a new database or overrides it if it already exists
+// It had to be it's own funtion, so that it can be called by multiple Tauri commands
+async fn create_or_override_database_pure(_file_path: &str) -> Result<(), Error> {
+    Sqlite::create_database(_file_path)
+        .await
+        .or(Err(Error::CreateDatabaseError))
+}
+
+#[derive(Debug)]
+pub enum Error {
+    DatabaseExistsError,
+    CreateDatabaseError,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl serde::Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,10 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![database::create_database])
+        .invoke_handler(tauri::generate_handler![
+            database::create_database,
+            database::create_or_override_database
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
