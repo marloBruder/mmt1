5a84d1ca4def834786c8796c741ec79ce9d2216f
Marlo Bruder <m.b.8@web.de>
2024-12-12 21:23:30 +0100

    Store db connection in app state

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,35 +1,54 @@
-use sqlx::{migrate::MigrateDatabase, Sqlite};
+use sqlx::{migrate::MigrateDatabase, Connection, Sqlite, SqliteConnection};
 use std::fmt;
+use tauri::async_runtime::Mutex;
+
+use crate::AppState;
 
 // Tauri Command for creating a new database
 // If the database already exists, it will instead return an DatabaseExistsError
 #[tauri::command]
-pub async fn create_database(file_path: &str) -> Result<(), Error> {
+pub async fn create_database(
+    file_path: &str,
+    state: tauri::State<'_, Mutex<AppState>>,
+) -> Result<(), Error> {
     //println!("Trying to create db with path {}!", file_path);
     if Sqlite::database_exists(file_path).await.unwrap_or(false) {
         return Err(Error::DatabaseExistsError);
     }
-    create_or_override_database_pure(file_path).await
+    create_or_override_database_pure(file_path, state).await
 }
 
 // Tauri Command for creating a new database or overriding it if it already exists
 #[tauri::command]
-pub async fn create_or_override_database(file_path: &str) -> Result<(), Error> {
-    create_or_override_database_pure(file_path).await
+pub async fn create_or_override_database(
+    file_path: &str,
+    state: tauri::State<'_, Mutex<AppState>>,
+) -> Result<(), Error> {
+    create_or_override_database_pure(file_path, state).await
 }
 
 // The function that actually creates a new database or overrides it if it already exists
 // It had to be it's own funtion, so that it can be called by multiple Tauri commands
-async fn create_or_override_database_pure(_file_path: &str) -> Result<(), Error> {
-    Sqlite::create_database(_file_path)
+async fn create_or_override_database_pure(
+    file_path: &str,
+    state: tauri::State<'_, Mutex<AppState>>,
+) -> Result<(), Error> {
+    Sqlite::create_database(file_path)
         .await
-        .or(Err(Error::CreateDatabaseError))
+        .or(Err(Error::CreateDatabaseError))?;
+    let con = SqliteConnection::connect(file_path)
+        .await
+        .or(Err(Error::ConnectDatabaseError))?;
+    let mut app_state = state.lock().await;
+    app_state.db_con = Some(con);
+    Ok(())
 }
 
 #[derive(Debug)]
 pub enum Error {
     DatabaseExistsError,
     CreateDatabaseError,
+    ConnectDatabaseError,
 }
 
 impl fmt::Display for Error {
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,16 @@
+use sqlx::SqliteConnection;
+use tauri::{async_runtime::Mutex, App, Manager};
+
 mod database;
+
+pub struct AppState {
+    db_con: Option<SqliteConnection>,
+}
+
+fn app_setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+    app.manage(Mutex::new(AppState { db_con: None }));
+    Ok(())
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -9,6 +21,7 @@ pub fn run() {
             database::create_database,
             database::create_or_override_database
         ])
+        .setup(|app| app_setup(app))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
