34f63a0e6c52e1390f248446bfc446a31d8983f2
Marlo Bruder <m.b.8@web.de>
2024-12-24 11:08:03 +0100

    Fixed tabBar bug

--- src/routes/main/+layout.svelte
+++ src/routes/main/+layout.svelte
@@ -6,23 +6,39 @@
   let { children }: { children: Snippet } = $props();
 </script>
 
-<div class="h-screen w-screen">
-  <div class="h-full w-80 fixed">
+<div class="h-screen w-screen custom-grid-layout fixed">
+  <div class="sideBar border-r border-gray-400">
     <NavSidebar></NavSidebar>
   </div>
-  <div class="h-full ml-80 border-l border-gray-400 custom-grid-layout">
-    <div class="h-8">
-      <TabBar></TabBar>
-    </div>
-    <div class="overflow-auto">
-      {@render children()}
-    </div>
+  <div class="tabBar overflow-hidden">
+    <TabBar></TabBar>
   </div>
+  <div class="tab overflow-auto">
+    {@render children()}
+  </div>
+  <!-- <div class="h-full ml-80 border-l border-gray-400">
+  </div> -->
 </div>
 
 <style>
   .custom-grid-layout {
     display: grid;
-    grid-template-rows: 2rem;
+    grid-template-areas:
+      "sideBar tabBar"
+      "sideBar tab";
+    grid-template-columns: 20rem auto;
+    grid-template-rows: 2rem auto;
+  }
+
+  .sideBar {
+    grid-area: sideBar;
+  }
+
+  .tabBar {
+    grid-area: tabBar;
+  }
+
+  .tab {
+    grid-area: tab;
   }
 </style>
