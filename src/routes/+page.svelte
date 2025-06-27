b7719c37e5a487d53a513e2017e01beea986c203
Marlo Bruder <m.b.8@web.de>
2025-06-27 02:22:52 +0200

    Removed mmdbt start page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,80 +1,70 @@
-<script module>
-  let resetApp = () => {
-    nameListData.resetLists();
-    explorerData.resetExplorer();
-    tabManager.resetTabs();
-    htmlData.resetHtmlData();
-  };
-  let loadApp = async () => {
-    await nameListData.load();
-    await htmlData.load();
-  };
-  export { resetApp, loadApp };
-</script>
-
 <script lang="ts">
-  import { goto } from "$app/navigation";
-  import { invoke } from "@tauri-apps/api/core";
-  import { save, confirm, open } from "@tauri-apps/plugin-dialog";
+  import NavSidebar from "$lib/components/nav/navSidebar/NavSidebar.svelte";
+  import TabBar from "$lib/components/nav/tabBar/TabBar.svelte";
+  import TitleBar from "$lib/components/titleBar/TitleBar.svelte";
+  import EmptyTabComponent from "$lib/components/tabs/EmptyTabComponent.svelte";
   import { tabManager } from "$lib/sharedState/tabManager.svelte";
-  import { nameListData } from "$lib/sharedState/nameListData.svelte";
-  import { explorerData } from "$lib/sharedState/explorerData.svelte";
-  import { htmlData } from "$lib/sharedState/htmlData.svelte";
-  import { onMount } from "svelte";
 
-  let createNewDB = async () => {
-    // Allow user to select file location
-    const filePath = await save({ filters: [{ name: "Metamath SQLite Database", extensions: ["mm.sqlite", "mm.db"] }] });
-    if (filePath) {
-      invoke("create_database", { filePath })
-        .then(() => {
-          resetApp();
-          goto("/main");
-        })
-        .catch(async (error) => {
-          if (error == "DatabaseExistsError") {
-            let confirmed = await confirm("You are about to override and delete an existing database. Are you sure?", { title: "Warning (mmdbt)", kind: "warning" });
-            if (confirmed) {
-              invoke("create_or_override_database", { filePath }).then(() => {
-                resetApp();
-                goto("/main");
-              });
-            }
-          } else {
-            throw error;
-          }
-        });
+  let openTab = $derived(tabManager.getOpenTab());
+
+  $effect(() => {
+    if (openTab) {
+      document.getElementById("tabContainer")!.scrollTop = openTab.scrollTop;
     }
-  };
-
-  let openDB = async () => {
-    const filePath = await open({ multiple: false, directory: false, filters: [{ name: "Metamath SQLite Database", extensions: ["mm.sqlite", "mm.db"] }] });
-
-    if (filePath) {
-      invoke("open_database", { filePath }).then(async (metamathDataUnknown) => {
-        resetApp();
-        await loadApp();
-        goto("/main");
-      });
-    }
-  };
-
-  onMount(() => {
-    goto("/main");
   });
+
+  let onscrollTab = (e: UIEvent) => {
+    if (openTab) {
+      openTab.scrollTop = (e.target as HTMLElement).scrollTop;
+    }
+  };
 </script>
 
-<main>
-  <div class="m-40 text-center">
-    <h1 class="text-4xl">Welcome to mmdbt!</h1>
-    <div>
-      <button onclick={createNewDB} class="inline-block mt-4">Create new Metamath database</button>
-    </div>
-    <div>
-      <button onclick={openDB} class="inline-block mt-4">Open Metamath database</button>
-    </div>
-    <div>
-      <a href="/import" class="inline-block mt-4">Import Metamath (.mm) file</a>
-    </div>
+<div class="h-screen w-screen custom-grid-layout fixed">
+  <div class="titleBar border-b border-black overflow-hidden">
+    <TitleBar></TitleBar>
   </div>
-</main>
+  <div class="sideBar border-r border-gray-400 overflow-hidden">
+    <NavSidebar></NavSidebar>
+  </div>
+  <div class="tabBar overflow-hidden">
+    <TabBar></TabBar>
+  </div>
+  <div id="tabContainer" class="tab overflow-auto" onscroll={onscrollTab}>
+    {#if openTab != null}
+      <openTab.component tab={openTab}></openTab.component>
+    {:else}
+      <EmptyTabComponent></EmptyTabComponent>
+    {/if}
+  </div>
+  <!-- <div class="h-full ml-80 border-l border-gray-400">
+  </div> -->
+</div>
+
+<style>
+  .custom-grid-layout {
+    display: grid;
+    grid-template-areas:
+      "titleBar titleBar"
+      "sideBar tabBar"
+      "sideBar tab";
+    grid-template-columns: 20rem auto;
+    grid-template-rows: 2rem 2rem auto;
+  }
+
+  .titleBar {
+    grid-area: titleBar;
+  }
+
+  .sideBar {
+    grid-area: sideBar;
+  }
+
+  .tabBar {
+    grid-area: tabBar;
+  }
+
+  .tab {
+    grid-area: tab;
+  }
+</style>
