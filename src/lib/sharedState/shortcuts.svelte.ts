a8fc2c33d21e2628f70b577be2c357673175128c
Marlo Bruder <m.b.8@web.de>
2025-09-01 15:50:40 +0200

    Close open tab shortcut

--- src/lib/sharedState/shortcuts.svelte.ts
+++ src/lib/sharedState/shortcuts.svelte.ts
@@ -1,7 +1,13 @@
+import { tabManager } from "./tabManager.svelte";
+
 export function setupShortcuts() {
   document.addEventListener("keydown", (e) => {
     if (e.ctrlKey && e.key == "r") {
       e.preventDefault();
+    } else if (e.ctrlKey && e.key == "p") {
+      e.preventDefault();
+    } else if (e.ctrlKey && e.key == "w") {
+      tabManager.closeOpenTab();
     }
   });
 }
