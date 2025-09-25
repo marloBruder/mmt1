604dcf7ac8a9f4eee9d957bddb3b5ecaf3481611
Marlo Bruder <m.b.8@web.de>
2025-09-25 22:57:59 +0200

    Render work variables with unicode

--- src/lib/components/util/MetamathExpression.svelte
+++ src/lib/components/util/MetamathExpression.svelte
@@ -5,7 +5,20 @@
 
   let symbols = $derived(expression.split(" "));
 
-  let htmlReps = $derived(symbols.map((symbol) => htmlData.getHtml(symbol)));
+  let htmlReps = $derived(
+    symbols.map((symbol) => {
+      if (symbol.includes("$")) {
+        let htmlOpt = htmlData.getHtml(symbol.split("$")[0]);
+        if (htmlOpt === undefined) {
+          return undefined;
+        }
+        let [htmlRep, typecodeNum] = htmlOpt;
+        return [htmlRep + "$" + symbol.split("$")[1], typecodeNum];
+      } else {
+        return htmlData.getHtml(symbol);
+      }
+    })
+  );
 </script>
 
 {#each htmlReps as htmlRep, i}
@@ -14,7 +27,7 @@
   {/if}
   {#if htmlRep}
     <span class={"math " + (htmlRep[1] !== 0 ? " custom-variable-color-" + htmlRep[1] : "")}>
-      {@html htmlRep[0]}{" "}
+      <span>{@html htmlRep[0]}</span>{" "}
     </span>
   {:else}
     {symbols[i]}
