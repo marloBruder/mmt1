be50b0f96cffd5134c6555d0dbc300587e0fe9fa
Marlo Bruder <m.b.8@web.de>
2025-10-19 17:36:14 +0200

    Maintain cursor position after unify/renumber/format

--- src/lib/components/tabs/EditorTabComponent.svelte
+++ src/lib/components/tabs/EditorTabComponent.svelte
@@ -9,7 +9,7 @@
 
     #filePath: string = $state("");
     #fileName: string = $derived.by(() => {
-      let segments = this.#filePath.split("\\");
+      const segments = this.#filePath.split("\\");
       return segments[segments.length - 1];
     });
     #isMmpFile: boolean = $derived(this.#fileName.endsWith(".mmp"));
@@ -28,7 +28,7 @@
     }
 
     async loadData(): Promise<void> {
-      let text = (await invoke("open_file", { relativePath: this.#filePath })) as string;
+      const text = (await invoke("open_file", { relativePath: this.#filePath })) as string;
       this.#monacoModel = monaco.editor.createModel(text, this.#isMmpFile ? "mmp" : "text");
       this.#monacoModel.onDidChangeContent(async () => {
         this.textChanged = true;
@@ -74,10 +74,10 @@
         return;
       }
 
-      let resultText = (await invoke("unify", { text: this.monacoModel!.getValue() })) as string | null;
+      const resultText = (await invoke("unify", { text: this.monacoModel!.getValue() })) as string | null;
 
       if (resultText !== null) {
-        editor.executeEdits("unifier", [{ range: new monaco.Range(1, 1, 1000000, 1), text: resultText, forceMoveMarkers: true }]);
+        changeEditorTextMaintainCursor(resultText);
       }
     }
 
@@ -114,9 +114,9 @@
         return;
       }
 
-      let resultText = (await invoke("format", { text: this.#monacoModel!.getValue() })) as string | null;
+      const resultText = (await invoke("format", { text: this.#monacoModel!.getValue() })) as string | null;
       if (resultText !== null) {
-        editor.executeEdits("format", [{ range: new monaco.Range(1, 1, 1000000, 1), text: resultText, forceMoveMarkers: true }]);
+        changeEditorTextMaintainCursor(resultText);
       }
     }
 
@@ -129,9 +129,9 @@
         return;
       }
 
-      let resultText = (await invoke("renumber", { text: this.#monacoModel!.getValue() })) as string | null;
+      const resultText = (await invoke("renumber", { text: this.#monacoModel!.getValue() })) as string | null;
       if (resultText !== null) {
-        editor.executeEdits("renumber", [{ range: new monaco.Range(1, 1, 10000, 1), text: resultText, forceMoveMarkers: true }]);
+        changeEditorTextMaintainCursor(resultText);
       }
     }
 
@@ -152,24 +152,13 @@
           errors: DetailedError[];
         }
 
-        let onEditData = onEditDataUnkown as OnEditData;
+        const onEditData = onEditDataUnkown as OnEditData;
 
         this.splitViewPageData = onEditData.pageData;
 
         if (tabManager.splitTabState === "externalWindow") {
           await emit("split-view-page-data-transfer", tabManager.getOpenTab()?.splitViewPageData);
         }
-
-        // const markers: Monaco.editor.IMarkerData[] = [
-        //   {
-        //     severity: monaco.MarkerSeverity.Error,
-        //     startLineNumber: 1,
-        //     startColumn: 1,
-        //     endLineNumber: 1,
-        //     endColumn: 5,
-        //     message: "Testing",
-        //   },
-        // ];
 
         const markers: Monaco.editor.IMarkerData[] = onEditData.errors.map((detailedError) => {
           return {
@@ -208,6 +197,16 @@
 
     get monacoScrollLeft() {
       return this.#monacoScrollLeft;
+    }
+  }
+
+  function changeEditorTextMaintainCursor(newtext: string) {
+    const selection = editor.getSelection();
+    const cursorPosition = selection?.getPosition();
+
+    editor.executeEdits("unifier", [{ range: new monaco.Range(1, 1, 1000000, 1), text: newtext, forceMoveMarkers: true }]);
+    if (cursorPosition !== undefined) {
+      editor.setPosition(cursorPosition);
     }
   }
 </script>
@@ -321,136 +320,8 @@
       unlisten();
     }
   });
-
-  // let lines = $derived.by(() => {
-  //   let lines = 1;
-  //   for (let char of editorTab.text) {
-  //     if (char === "\n") {
-  //       lines++;
-  //     }
-  //   }
-  //   return lines;
-  // });
-
-  let nameInput: string = $state("");
-
-  let nameDisabled: boolean = $state(true);
-
-  $effect(() => {
-    if (!nameDisabled) {
-      let input = document.getElementById("tabName");
-      if (input) {
-        input.focus();
-      }
-    }
-  });
-
-  let editName = () => {
-    nameInput = editorTab.fileName;
-    nameDisabled = false;
-  };
-
-  let saveName = async () => {
-    // invoke("set_in_progress_theorem_name", { oldName, newName: theorem.name }).then(() => {
-    //   nameDisabled = true;
-    //   if (oldName != theorem.name) {
-    //     nameListData.changeInProgressTheoremName(oldName, theorem.name);
-    //     editorTab.changeEditorID(theorem.name);
-    //   }
-    // });
-    nameDisabled = true;
-  };
-
-  let abortNameSave = () => {
-    nameDisabled = true;
-    nameInput = editorTab.fileName;
-  };
-
-  let onFocusOutName = async () => {
-    try {
-      await saveName();
-    } catch (error) {
-      abortNameSave();
-    }
-  };
-
-  let onkeyDownName = (event: KeyboardEvent) => {
-    if (event.key == "Enter") {
-      try {
-        saveName();
-      } catch (error) {}
-    } else if (event.key == "Escape") {
-      abortNameSave();
-    }
-  };
-
-  let textChange = () => {
-    tabManager.makeOpenTempTabPermanent();
-    editorTab.textChanged = true;
-  };
-
-  let textareaKeyDown = async (e: KeyboardEvent) => {
-    if (e.key === "u" && e.ctrlKey) {
-      e.preventDefault();
-      let textarea = document.getElementById("editorTextarea") as HTMLTextAreaElement;
-      let resultText = (await invoke("unify", { text: textarea.value, cursorPos: textarea.selectionStart })) as string;
-      if (resultText != editorTab.monacoModel?.getValue()) {
-        editorTab.monacoModel?.setValue(resultText);
-        // editorTab.textChanged = true;
-      }
-    }
-  };
-
-  let addToDatabase = () => {
-    editorTab.addToDatabase();
-  };
-
-  let placeAfter = $state("");
-
-  // $effect(() => {
-  //   let textarea = document.getElementById("editorTextarea");
-  //   if (textarea) {
-  //     textarea.style.height = lines * 1.5 + "rem";
-  //   }
-  // });
-
-  let belowTextareaClick = () => {
-    let textarea = document.getElementById("editorTextarea");
-    if (textarea && textarea instanceof HTMLTextAreaElement) {
-      textarea.focus();
-      textarea.setSelectionRange(-1, -1);
-    }
-  };
 </script>
 
-<!-- <div class="m-2">
-  <div class="mb-2">
-    <label for="tabName">Theorem name:</label>
-    <input id="tabName" type="text" bind:value={nameInput} onfocusout={onFocusOutName} onkeydown={onkeyDownName} disabled={nameDisabled} autocomplete="off" class="disabled:bg-gray-300" />
-  </div>
-  <RoundButton onclick={editName} disabled={!nameDisabled}>Edit name</RoundButton>
-</div>
-<div class="p-2 border-t border-gray-400">
-  <RoundButton onclick={saveText} disabled={!editorTab.textChanged}>Save</RoundButton>
-  <RoundButton onclick={deleteTheorem} warning>Delete</RoundButton>
-</div>
-<div class="p-2 border-t border-gray-400">
-  <label for="placeAfter">Place after:</label>
-  <input id="placeAfter" bind:value={placeAfter} autocomplete="off" />
-  <RoundButton onclick={addToDatabase}>Add to database</RoundButton>
-</div> -->
 <div class="h-full w-full">
-  <div id="editor-area" class="h-full w-full">
-    <!-- <div class="w-8 float-left text-right">
-    {#each { length: lines } as _, i}
-      <div>
-        {i}
-      </div>
-    {/each}
-  </div>
-  <div class="ml-12">
-    <textarea id="editorTextarea" bind:value={editorTab.text} oninput={textChange} onkeydown={textareaKeyDown} class="w-full resize-none h-96 text-nowrap overflow-x-hidden focus:outline-none" spellcheck="false"></textarea>
-    <button class="w-full h-screen cursor-text" onclick={belowTextareaClick} aria-label="below-textrea"></button>
-  </div> -->
-  </div>
+  <div id="editor-area" class="h-full w-full"></div>
 </div>
